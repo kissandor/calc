1098554069a20092e6d811ea1e96b0c383f1b15d
Kis Sandor <kissandor@hotmail.com>
2020-09-19 22:51:24 +0100

    claculating - still work needs to be done

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ let topScreen = document.querySelector(".top-screen");
 let mainScreen = document.querySelector(".main-screen");
 let btnNumbers = document.querySelectorAll(".numbers");
 let isCommaClicked = false;
+let operands = document.querySelectorAll(".operand");
+
+//variables to store the numbers
+let firstNumber = undefined;
+let secondNumber = 0;
+let operandToExecute = undefined;
+let resoult = 0;
 
 //clear Button, clear the top and the main screen.
 let ac = () => {
@@ -21,6 +28,7 @@ let displayNumbers = (btn) => {
       mainScreen.innerHTML = "";
       mainScreen.innerHTML += btn.innerHTML;
     } else if (mainScreen.innerHTML == "0" && btn.innerHTML == 0) {
+      return;
       //does nothing if screen is 0 and only pressing the 0 button
     } else {
       mainScreen.innerHTML += btn.innerHTML;
@@ -37,5 +45,48 @@ comma.addEventListener("click", () => {
   if (!isCommaClicked) {
     mainScreen.innerHTML += comma.innerHTML;
     isCommaClicked = true;
+  }
+});
+
+//clicking on the operands
+operands.forEach((operand) => {
+  operand.addEventListener("click", () => {
+    if (mainScreen.innerHTML != "") {
+      firstNumber = parseInt(mainScreen.innerHTML);
+    }
+    if (firstNumber != undefined) {
+      topScreen.innerHTML = firstNumber + " " + operand.innerHTML;
+      operandToExecute = operand.innerHTML;
+      mainScreen.innerHTML = "";
+    }
+    secondNumber = firstNumber;
+    firstNumber = undefined;
+  });
+});
+
+//calculating
+let calcButton = document.querySelector(".equal");
+calcButton.addEventListener("click", () => {
+  switch (operandToExecute) {
+    case "+":
+      resoult = secondNumber + parseInt(mainScreen.innerHTML);
+      mainScreen.innerHTML = resoult;
+      topScreen.innerHTML = "";
+      break;
+    case "-":
+      resoult = secondNumber - parseInt(mainScreen.innerHTML);
+      mainScreen.innerHTML = resoult;
+      topScreen.innerHTML = "";
+      break;
+    case "x":
+      resoult = secondNumber * parseInt(mainScreen.innerHTML);
+      mainScreen.innerHTML = resoult;
+      topScreen.innerHTML = "";
+      break;
+    case "/":
+      resoult = secondNumber / parseInt(mainScreen.innerHTML);
+      mainScreen.innerHTML = resoult;
+      topScreen.innerHTML = "";
+      break;
   }
 });
